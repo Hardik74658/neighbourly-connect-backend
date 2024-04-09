f69a4d119cce154b69f2673057d1b5afbf0c9239
Hardik74658 <127240209+Hardik74658@users.noreply.github.com>
2024-04-09 22:05:12 +0530

    utility classes and functions are added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import dotenv from "dotenv"
 import { connectDB } from "./db/index.js";
-
+import { app } from "./app.js";
 
 dotenv.config({
         path:"../env"
     })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("App is Listening On Port :: ",process.env.PORT || 8000);
+    })
+    app.on("error",(error)=>{
+        console.log("Error Occured :: ",error);
+        throw error;
+    })
+})
+.catch((error)=>{
+    console.log("MONGO DB Connection Eroor :: ",error);  
+})
 
